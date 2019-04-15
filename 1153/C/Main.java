5a3aa1e00145fb7793af733d2921c1cde7ea5ba6
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-04-15 16:46:00 +0700

    1153 C

--- 1153/C/Main.java
+++ 1153/C/Main.java
@@ -9,14 +9,60 @@ public class Main {
     int n = in.nextInt();
     in.nextLine();
 
-    String seq = in.nextLine();
-
-    if (seq.charAt(0) == ')' || seq.charAt(seq.length()) == '(') {
+    char[] ans = in.nextLine().toCharArray();
+    boolean valid = true;
+    // out.println(n);
+    if (n % 2 != 0) {
       out.println(":(");
-    } else {
-      for (int i = 0; i < max; i++) {
-        
+      valid = false;
+    }
+    int l = n >> 1;
+    int r = n >> 1;
+    for (int i = 0; i < n; i++) {
+      if (ans[i] == '(') l--;
+      if (ans[i] == ')') r--;
+    }
+    if (valid && (l < 0 || r < 0)) {
+      out.println(":(");
+      valid = false;
+    }
+    // out.println(ans[0]);
+    if (valid && (ans[0] == ')' || ans[n-1] == '(')) {
+      out.println(":(");
+      valid = false;
+    }
+
+    int parPoint = 0;
+    
+    for (int i = 0; i < n && valid; i++) {
+      if (ans[i] == '(') {
+        parPoint++;
       }
+      if (ans[i] == ')') {
+        parPoint--;
+      }
+      if (ans[i] == '?') {
+        if (l > 0) {
+          ans[i] = '(';
+          l--;
+          parPoint++;
+        } else {
+          if (r > 0) {
+            ans[i] = ')';
+            r--;
+            parPoint--;
+          }
+        }
+      }
+      if (parPoint == 0 && i != (n - 1)) {
+        out.println(":(");
+        valid = false;
+        break;
+      }
+    }
+
+    if (valid) {
+      out.println(ans);
     }
 
     in.close();

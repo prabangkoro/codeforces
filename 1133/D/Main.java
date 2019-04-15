b757a1453720d7fb7f01c6fc55f020f52cc10b49
bagus prabangkoro <bagusprabangkoro@yahoo.co.id>
2019-04-15 23:41:57 +0700

    fractions

--- 1133/D/Main.java
+++ 1133/D/Main.java
@@ -4,80 +4,45 @@ import java.util.*;
 public class Main {
   static Scanner in = new Scanner(System.in);
   static PrintWriter out = new PrintWriter(System.out);
+  static int[] counter = new int[(int)2e5+5];
 
-  static int gcd (int a, int b) {
+  static long gcd (long a, long b) {
     if (b == 0) {
       return a;
     }
     return gcd(b, a % b);
   }
 
-  static long generateCombinedInteger (int numerator, int denominator) {
-    int num = numerator;
-    int den = denominator;
-    if (den < 0) {
-      den *= -1;
-      num *= -1;
-    }
-    if (den != 0) {
-      int gcd = gcd(Math.abs(num), Math.abs(den));
-      num /= gcd;
-      den /= gcd;
-    }
-    return (num << 32) | den;
-  }
-  
   public static void main(String[] args) {
     int n = in.nextInt();
-    int[] a = new int[n];
-    int[] b = new int[n];
+    long[] a = new long[n];
+    long[] b = new long[n];
 
-    for (int i = 0; i < n; i++) {
-      a[i] = in.nextInt();
-    }
-    for (int i = 0; i < n; i++) {
-      b[i] = in.nextInt();
-    }
+    for (int i = 0; i < n; i++) a[i] = in.nextLong();
+    for (int i = 0; i < n; i++) b[i] = in.nextLong();
 
-    boolean[] zeroImpossible = new boolean[n];
-    int zeroImpossibleCount = 0;
+    HashMap<String, Integer> fractions = new HashMap<>();
+    int idx = 0;
+    int base = 0;
+    int ans = 0;
     for (int i = 0; i < n; i++) {
-      if (a[i] == 0 && b[i] != 0) {
-        zeroImpossible[i] = true;
-        zeroImpossibleCount++;
-      }
-    }
-
-    if (zeroImpossibleCount == n) {
-      out.println(0);
-    } else {
-      ArrayList<Long> hashAB = new ArrayList<>();
-      
-      for (int i = 0; i < n; i++) {
-        if (!zeroImpossible[i]) {
-          // b_a[i] = generateCombinedInteger(b[i], a[i]);
-          hashAB.add(generateCombinedInteger(b[i], a[i]));
-          // out.println(b_a[i]);
+      if (a[i] == 0) {
+        if (b[i] == 0) {
+          base++;
         }
+        continue;
       }
-  
-      Collections.sort(hashAB);
-  
-      int counterMax = 1;
-      int counter = 1;
-      long prev = hashAB.get(0);
-      for (int i = 1; i < n; i++) {
-        if (prev == hashAB.get(i)) {
-          counter++;
-          counterMax = Math.max(counterMax, counter);
-        } else {
-          counter = 0;
-        }
+      long gcd = gcd(a[i], b[i]);
+      a[i] /= gcd;
+      b[i] /= gcd;
+      String fraction = b[i] + "/" + a[i];
+      if (!fractions.containsKey(fraction)) {
+        fractions.put(fraction, idx++);
       }
-  
-      out.println(counterMax);
+      ans = Math.max(ans, ++counter[fractions.get(fraction)]);
     }
 
+    out.println(ans + base);
 
     in.close();
     out.close();

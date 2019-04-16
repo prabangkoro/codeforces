8cf1e796405a9cb7d56196730baebd4701e03b95
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-04-16 15:47:28 +0700

    1132C on progress

--- 1132/C/Main.java
+++ 1132/C/Main.java
@@ -1,48 +1,96 @@
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        PrintWriter out = new PrintWriter(System.out);
-
-        int n = scanner.nextInt();
-        int q = scanner.nextInt();
-
-        ArrayList<ArrayList<Integer>> fencePainters = new ArrayList<ArrayList<Integer>>(n);
-        int[] fencePainterAmount = new int[n];
-
-        for (int i = 0; i < q; i++) {
-            int l = scanner.nextInt();
-            int r = scanner.nextInt();
-
-            for (int j = l - 1; j < r; j++) {
-                fencePainterAmount[j]++;
-                fencePainters.get(j).add(i);
-            }
-        }
-
-        int maxPainted = n;
-        out.println("max painted: ");
-        for (int i = 0; i < n; i++) {
-            out.println(fencePainterAmount[i]);
-            if (fencePainterAmount[i] == 0) {
-                maxPainted--;
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            out.print(i + " : ");
-            ArrayList singleFencePainter = fencePainters.get(i);
-            for (int j = 0; j < singleFencePainter.size(); j++) {
-                out.print(singleFencePainter.get(j) + " ");
-            }
-            out.println();
-        }
-
-        out.println(maxPainted);
-        out.flush();
+  static Scanner in = new Scanner(System.in);
+  static PrintWriter out = new PrintWriter(System.out);
+  
+  static int getMinRes (int[] l, int[] r) {
+    int[] painterCountChange = new int[n + 1];
+    for (int i = 0; i < q; i++) {
+      painterCountChange[l[i]]++;
+      painterCountChange[r[i]]--;
     }
+
+    int[] painterCountList = new int[n];
+    int painterCount = 0;
+    for (int i = 0; i < n; i++) {
+      painterCount += painterCountChange[i];
+      painterCountList[i] = painterCount;
+    }
+
+    int[] d = new int[n];
+    for (int i = 0; i < n; i++) {
+      if (painterCountList[i] != 1) continue;
+      d[i] = 1;
+    }
+    
+    int sum = 0;
+    int[] prefixSum = new int[n + 1];
+    for (int i = 0; i < n; i++) {
+      sum += d[i];
+      prefixSum[i + 1] = sum;
+    }
+    
+    int minRes = n + 1;
+    int qWithMinRes = -1;
+    for (int i = 0; i < q; i++) {
+      int res = prefixSum[r[i]] - prefixSum[l[i]];
+      if (minRes > res) {
+        qWithMinRes = i;
+      }
+      minRes = Math.min(res, minRes);
+    }
+    return 0;
+  }
+  public static void main(String[] args) {
+    int n = in.nextInt();
+    int q = in.nextInt();
+
+    int[] l = new int[q];
+    int[] r = new int[q];
+    int[] painterCountChange = new int[n + 1];
+    for (int i = 0; i < q; i++) {
+      l[i] = in.nextInt() - 1;
+      r[i] = in.nextInt();
+      painterCountChange[l[i]]++;
+      painterCountChange[r[i]]--;
+    }
+
+    int[] painterCountList = new int[n];
+    int painterCount = 0;
+    for (int i = 0; i < n; i++) {
+      painterCount += painterCountChange[i];
+      painterCountList[i] = painterCount;
+    }
+
+    int[] d = new int[n];
+    for (int i = 0; i < n; i++) {
+      if (painterCountList[i] != 1) continue;
+      d[i] = 1;
+    }
+    
+    int sum = 0;
+    int[] prefixSum = new int[n + 1];
+    for (int i = 0; i < n; i++) {
+      sum += d[i];
+      prefixSum[i + 1] = sum;
+    }
+    
+    int minRes = n + 1;
+    int qWithMinRes = -1;
+    for (int i = 0; i < q; i++) {
+      int res = prefixSum[r[i]] - prefixSum[l[i]];
+      if (minRes > res) {
+        qWithMinRes = i;
+      }
+      minRes = Math.min(res, minRes);
+    }
+
+    out.println("qWithMinRes : " + qWithMinRes);
+    out.println("minRes : " + minRes);
+
+    in.close();
+    out.close();
+  }
 }

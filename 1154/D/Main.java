cea99781d41c282b58e4a00fdf1eb71244982494
bagus prabangkoro <bagusprabangkoro@yahoo.co.id>
2019-04-18 13:32:39 +0700

    1154 D done

--- 1154/D/Main.java
+++ 1154/D/Main.java
@@ -6,6 +6,43 @@ public class Main {
   static PrintWriter out = new PrintWriter(System.out);
   
   public static void main(String[] args) {
+    int n = in.nextInt();
+    int b = in.nextInt();
+    int a = in.nextInt();
+    int maxCapA = a;
+
+    int distance = 0;
+    for (int i = 0; i < n; i++) {
+      int exposed = in.nextInt();
+      // out.println("exposed: " + exposed + " b: " + b + " a:" + a);
+      if (b == 0 && a == 0) break;
+      if (exposed == 0) {
+        if (a > 0) {
+          a--;
+          distance++;
+        } else {
+          b--;
+          distance++;
+        }
+      } else {
+        if (a == maxCapA) {
+          a--;
+          distance++;
+          continue;
+        }
+        if (b > 0) {
+          b--;
+          a = Math.min(a + 1, maxCapA);
+          distance++;
+        } else {
+          a--;
+          distance++;
+        }
+      }
+      // out.println("distance: " + distance);
+    }
+
+    out.println(distance);
 
     in.close();
     out.close();
--- 1154/E/Main.java
+++ 1154/E/Main.java
@@ -6,6 +6,26 @@ public class Main {
   static PrintWriter out = new PrintWriter(System.out);
   
   public static void main(String[] args) {
+    int n = in.nextInt();
+    int k = in.nextInt();
+
+    LinkedList<Integer> students = new LinkedList<>();
+    for (int i = 0; i < n; i++) {
+      students.add(in.nextInt());
+    }
+
+    int maxSkill = 0;
+    int maxSkillIndex = 0;
+    int index = 0;
+    Iterator<Integer> iterator = students.iterator();
+    while (iterator.hasNext()) {
+      int skill = iterator.next();
+      if (skill > maxSkill) {
+        maxSkillIndex = index;
+        maxSkill = skill;
+      }
+      index++;
+    }
 
     in.close();
     out.close();

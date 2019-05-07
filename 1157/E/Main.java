9530c74410d03e70376b3e5a65d676cb777681c7
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-05-07 14:17:45 +0700

    using treemap

--- 1157/E/Main.java
+++ 1157/E/Main.java
@@ -9,8 +9,7 @@ public class Main {
     int n = in.nextInt();
 
     int[] conj_a = new int[n];
-    int[] b = new int[200001];
-    PriorityQueue<Integer> b_pq = new PriorityQueue<>();
+    TreeMap<Integer, Integer> b_tm = new TreeMap<>();
 
     for (int i = 0; i < n; i++) {
       int input = in.nextInt();
@@ -19,23 +18,38 @@ public class Main {
 
     for (int i = 0; i < n; i++) {
       int input = in.nextInt();
-      b[input]++;
-      b_pq.add(input);
+      if (b_tm.containsKey(input)) {
+        int count = b_tm.get(input) + 1;
+        b_tm.replace(input, count);
+      } else {
+        b_tm.put(input, 1);
+      }
     }
 
     int[] ans = new int[n];
+
     for (int i = 0; i < n; i++) {
-      if (b[conj_a[i]] > 0) {
-        b[conj_a[i]]--;
+      int min_b = b_tm.firstKey();
+      int max_b = b_tm.lastKey();
+      // out.println("conj a: " + conj_a[i] + " min b: " + min_b + " max b: " + max_b);
+      if (min_b > conj_a[i] || max_b < conj_a[i]) {
+        ans[i] = ((n - conj_a[i]) + min_b) % n;
+        int count = b_tm.get(min_b) - 1;
+        if (count == 0) {
+          b_tm.remove(min_b);
+        } else {
+          b_tm.replace(min_b, count);
+        }
       } else {
-        int counter = 1;
-        while (counter < n) {
-          if (b[(conj_a[i] + counter) % n] > 0) {
-            ans[i] = counter;
-            b[(conj_a[i] + counter) % n]--;
-            break;
-          }
-          counter++;
+        Map.Entry least = b_tm.ceilingEntry(conj_a[i]);
+        // out.println("least key: " + least.getKey());
+        ans[i] = (int)least.getKey() - conj_a[i];
+        // out.println("counter curr: " + least.getValue());
+        int count = (int)least.getValue() - 1;
+        if (count == 0) {
+          b_tm.remove(least.getKey());
+        } else {
+          b_tm.replace((int)least.getKey(), count);
         }
       }
     }
@@ -43,11 +57,12 @@ public class Main {
     for (int answer : ans) {
       out.print(answer + " ");
     }
-    
-    Iterator<Integer> it = b_pq.iterator();
-    while (it.hasNext()) {
-      out.println(it.next());
-    }
+
+    out.println();
+
+    // for (Map.Entry mapEntry : b_tm.entrySet()) {
+    //   out.println(mapEntry.getKey() + " " + mapEntry.getValue());
+    // }
 
     in.close();
     out.close();

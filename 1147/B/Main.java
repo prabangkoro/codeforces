cd86244cd2c1d7535945a755c541e015f7c80fa3
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-05-09 14:18:46 +0700

    chladni figure

--- 1147/B/Main.java
+++ 1147/B/Main.java
@@ -4,45 +4,56 @@ import java.util.*;
 public class Main {
   static Scanner in = new Scanner(System.in);
   static PrintWriter out = new PrintWriter(System.out);
+
+  static int gcd (int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+  }
   
   public static void main(String[] args) {
     int n = in.nextInt();
     int m = in.nextInt();
 
     int[][] segments = new int[m][2];
+    HashSet<Long> hash_segments = new HashSet<>();
     for (int i = 0; i < m; i++) {
-      segments[i][0] = in.nextInt();
-      segments[i][1] = in.nextInt();
+      int start_line = in.nextInt();
+      int end_line = in.nextInt();
+      segments[i][0] = Math.min(start_line, end_line);
+      segments[i][1] = Math.max(start_line, end_line);
+      hash_segments.add(((long)segments[i][0] << 32) | (long)segments[i][1]);
     }
 
-    boolean[] crawled = new boolean[m];
-    boolean symetrical = true;
-    for (int i = 0; i < m && symetrical; i++) {
-      if (crawled[i]) continue;
-      int diff = Math.abs(segments[i][0] - segments[i][1]);
-      diff = Math.min(n - diff, diff);
-      if (n % diff != 0) {
-        symetrical = false;
-        break;
-      }
-      for (int j = i; j < m; j++) {
-        if (crawled[j]) continue;
-        int curr_diff = Math.abs(segments[j][0] - segments[j][1]);
-        curr_diff = Math.min(n - curr_diff, curr_diff);
-        if (curr_diff != diff) continue;
-        if (Math.abs(segments[j][0] - segments[i][0]) % diff == 0) {
-          crawled[j] = true;
+    TreeSet<Integer> steps = new TreeSet<>();
+    for (int i = 1; i < n; i++) {
+      steps.add(gcd(i, n));
+    }
+
+    Iterator<Integer> it = steps.iterator();
+    boolean match = true;
+    while (it.hasNext()) {
+      int k = it.next();
+      for (int i = 0; i < m; i++) {
+        int a = segments[i][0] + k;
+        int b = segments[i][1] + k;
+        if (a > n) a -= n;
+        if (b > n) b -= n;
+        long hash_ab = ((long)Math.min(a, b) << 32) | (long)Math.max(a, b);
+        if (hash_segments.contains(hash_ab)) {
+          match = true;
+        } else {
+          match = false;
+          break;
         }
       }
+      if (match) break;
     }
 
-    for (int i = 0; i < m && symetrical; i++) {
-      if (!crawled[i]) {
-        symetrical = false;
-      }
+    if (match) {
+      out.println("Yes");
+    } else {
+      out.println("No");
     }
-
-    out.println(symetrical ? "Yes" : "No");
 
     in.close();
     out.close();

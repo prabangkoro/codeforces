dc15dc6d95421b873481ea63e377227c9f884870
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-05-10 10:14:23 +0700

    update 1163

--- 1163/A/Main.java
+++ 1163/A/Main.java
@@ -8,37 +8,11 @@ public class Main {
   public static void main(String[] args) {
     int n = in.nextInt();
     int m = in.nextInt();
-    int remaining = n - m;
-
-    int[] circle = new int[n];
-    for (int i = 0; i < n && remaining > 0; i++) {
-      if (i % 2 != 0) {
-        circle[i] = 1;
-        remaining--;
-      }
-    }
-
-    if (remaining > 0) {
-      for (int i = n - 1; i >= 0 && remaining > 0; i--) {
-        if (circle[i] != 1) {
-          circle[i] = 1;
-          remaining--;
-        }
-      }
-    }
-
-    int ans = 0;
-    // for (int i = 0; i < n; i++) {
-    //   out.println("i: " + i + " circle: " + circle[i]);
-    // }
-    for (int i = 0; i < n - 1; i++) {
-      if (circle[i] == 0 && circle[i + 1] == 1) ans++;
-    }
-
-    if (m == 0) {
-      out.println(1);
-    } else {
-      out.println(ans);
+    
+    if (m == 0) out.println(1);
+    else {
+      if (m <= (n / 2)) out.println(m);
+      else out.println(n - m);
     }
 
     in.close();
--- 1163/B2/Main.java
+++ 1163/B2/Main.java
@@ -4,44 +4,34 @@ import java.util.*;
 public class Main {
   static Scanner in = new Scanner(System.in);
   static PrintWriter out = new PrintWriter(System.out);
+
+  static int NMAX = (int)1e5 + 1;
+  static int[] frequency = new int[NMAX];
+  static int[] f_counter = new int[NMAX];
   
   public static void main(String[] args) {
     int n = in.nextInt();
-    int[] frequency = new int[100001];
-    int[] ui = new int[n];
-    int max_n = 0;
 
-    for (int i = 0; i < n; i++) {
+    int ans = 0;
+    int f_max = 0;
+    for (int i = 1; i <= n; i++) {
       int input = in.nextInt();
-      ui[i] = input;
+      f_counter[frequency[input]]--;
       frequency[input]++;
-      max_n = Math.max(input, max_n);
-    }
+      f_counter[frequency[input]]++;
+      f_max = Math.max(f_max, frequency[input]);
 
-    int ans = n;
-    for (int i = n - 1; i >= 0; i--) {
-      TreeSet<Integer> freqs = new TreeSet<>();
-      for (int j = 1; j <= max_n; j++) {
-        if (frequency[j] != 0) {
-          freqs.add(frequency[j]);
-        }
-      }
-      if (freqs.size() == 2) {
-        int p = 0, q = 0;
-        for (int j = 1; j <= max_n; j++) {
-          if (frequency[j] == freqs.first()) p++;
-          else if (frequency[j] == freqs.last()) q++;
-        }
-        if (p == 1 && (freqs.first() == 1 || freqs.first() == freqs.last() + 1)) {
-          ans = i + 1;
-          break;
-        }
-        if (q == 1 && (freqs.last() == 1 || freqs.first() + 1 == freqs.last())) {
-          ans = i + 1;
-          break;
-        }
-      }
-      frequency[ui[i]]--;
+      boolean ok = false;
+      // one color appears once
+      if (f_counter[i] == 1){ ok = true; }
+      // each color appears once
+      else if (f_counter[1] == i) { ok = true; }
+      // one color appears once, and the other have same frequency
+      else if (f_counter[1] == 1 && f_counter[f_max] * f_max == i - 1) { ok = true; }
+      // one color appears one more than the other. the other have same frequency
+      else if (f_counter[f_max] == 1 && f_counter[f_max - 1] * (f_max - 1) == (i - f_max)) { ok = true; }
+
+      if (ok) ans = i;
     }
 
     out.println(ans);

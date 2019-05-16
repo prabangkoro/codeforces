17a84197de5d57598799670b4bf53fab8de8b8e2
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-05-16 11:13:32 +0700

    party and sweets

--- 1159/C/Main.java
+++ 1159/C/Main.java
@@ -8,37 +8,52 @@ public class Main {
   public static void main(String[] args) {
     int n = in.nextInt();
     int m = in.nextInt();
-
-    ArrayList<Integer> boys = new ArrayList<>();
-    int min_boys = Integer.MAX_VALUE;
-    for (int i = 0; i < n; i++) {
-      int input = in.nextInt();
-      boys.add(input);
-      min_boys = Math.min(min_boys, input);
-    }
-    PriorityQueue<Integer> girls = new PriorityQueue<>();
-    int max_girls = 0;
-    for (int i = 0; i < m; i++) {
-      int input = in.nextInt();
-      girls.add(input);
-      max_girls = Math.max(max_girls, input);
-    }
+    long boyMax = 0;
+    int NBoyMax = 0;
 
     long sweets = 0;
-    if (min_boys > max_girls) {
-      sweets = -1;
-    } else {
-      Collections.sort(boys);
-      for (Integer boy : boys) {
-        girls.peek();
-        Iterator<Integer> it_girls = girls.iterator();
-        while (it_girls.hasNext()) {
+    TreeSet<Long> boyMember = new TreeSet<>();
+    for (int i = 0; i < n; i++) {
+      long input = in.nextLong();
+      boyMember.add(input);
+      if (boyMax < input) {
+        boyMax = input;
+        NBoyMax = 1;
+      } else if (boyMax == input) NBoyMax++;
+      sweets += (input * m);
+    }
 
+    long smallestGirl = (long) 1e8 + 1;
+    long sum = 0;
+    for (int i = 0; i < m; i++) {
+      long input = in.nextLong();
+      sum += input;
+      if (smallestGirl > input) smallestGirl = input;
+    }
+
+    if (smallestGirl < boyMember.last()) {
+      out.println(-1);
+    } else if (smallestGirl == boyMember.last()) {
+      sweets += sum - boyMember.last() * m;
+      out.println(sweets);
+    } else {
+
+      if (NBoyMax > 1) {
+        sweets += sum - boyMember.last() * m;
+        out.println(sweets);
+      } else {
+        Object[] boyList = boyMember.toArray();
+        if (boyList.length > 1) {
+          long boy = 0;
+          boy = (long)boyList[boyList.length - 2];
+          sweets += (sum - smallestGirl - boyMember.last() * (m - 1));
+          sweets += (smallestGirl - boy);
+          out.println(sweets);
+        } else {
+          out.println(-1);
         }
       }
     }
-
-    out.println(sweets);
 
     in.close();
     out.close();

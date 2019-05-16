9a4ed8c2a55e6fb7edc552e96de3543df6fdd45b
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-05-16 21:33:31 +0700

    Two Arrays and Sum of Functions

--- 1165/E/Main.java
+++ 1165/E/Main.java
@@ -4,8 +4,33 @@ import java.util.*;
 public class Main {
   static Scanner in = new Scanner(System.in);
   static PrintWriter out = new PrintWriter(System.out);
-  
+
+  static long MOD = 998244353;
+
   public static void main(String[] args) {
+    int n = in.nextInt();
+
+    long[] val = new long[n];
+    for (long i = 0; i < n; i++) {
+      long input = in.nextLong();
+      val[(int)i] = input * (i + 1) * ((long)n - i);
+    }
+    Arrays.sort(val);
+    
+    ArrayList<Long> b = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      b.add(in.nextLong());
+    }
+    Collections.sort(b);
+
+    long ans = 0;
+    for (int i = n - 1; i >= 0; i--) {
+      // out.println("i: " + i);
+      long incr = ((val[i] % MOD) * b.get(n - 1 - i)) % MOD;
+      ans = (ans + incr) % MOD;
+    }
+
+    out.println(ans);
 
     in.close();
     out.close();

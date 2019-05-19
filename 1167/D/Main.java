efd55203c1186413d921becaade56d0f0f2a8fe1
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-05-19 10:29:41 +0700

    1167 D

--- 1167/D/Main.java
+++ 1167/D/Main.java
@@ -6,6 +6,26 @@ public class Main {
   static PrintWriter out = new PrintWriter(System.out);
   
   public static void main(String[] args) {
+    int n = in.nextInt();
+    in.nextLine();
+    String sequence = in.nextLine();
+
+    int[] score = new int[n];
+    int current = 0;
+    int max = 0;
+    for (int i = 0; i < n; i++) {
+      if (sequence.charAt(i) == '(') {
+        score[i] = ++current;
+      } else {
+        score[i] = current--;
+      }
+      max = Math.max(max, current);
+    }
+
+    for (int i = 0; i < n; i++) {
+      int ans = score[i] > (max / 2) ? 1 : 0;
+      out.print(ans);
+    }
 
     in.close();
     out.close();

18a8e80a3fc1b98ba8d9470ce1ef0fe730d61f41
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-05-20 17:13:31 +0700

    retry

--- 1118/C/Main.java
+++ 1118/C/Main.java
@@ -5,60 +5,58 @@ public class Main {
   static Scanner in = new Scanner(System.in);
   static PrintWriter out = new PrintWriter(System.out);
   static int MAXN = 1001;
-  static int[] countMember = new int[MAXN];
-  
+  static int[] counter = new int[MAXN];
+
   public static void main(String[] args) {
     int n = in.nextInt();
 
-    for (int i = 0; i < n * n; i++) {
-      ++countMember[in.nextInt()];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        ++counter[in.nextInt()];
+      }
     }
 
     int[][] cellMapping = new int[(n + 1) / 2][(n + 1) / 2];
     for (int i = 0; i < (n + 1) / 2; i++) {
       for (int j = 0; j < (n + 1) / 2; j++) {
-        if (i != (n - 1 - i) && j != (n - 1 - j)) {
-          cellMapping[i][j] = 4;
-        } else if (i != (n - 1 - i) ^ j != (n - 1 - j)) {
-          cellMapping[i][j] = 2;
-        } else {
-          cellMapping[i][j] = 1;
-        }
+        if (i != (n - 1 - i) && j != (n - 1 - j)) cellMapping[i][j] = 4;
+        else if (i != (n - 1 - i) ^ j != (n - 1 - j)) cellMapping[i][j] = 2;
+        else cellMapping[i][j] = 1;
       }
     }
 
-    int[][] a = new int[n][n];
-    boolean undone = false;
-    for (int map : new int[] {4, 2, 1}) {
-      int counter = 1;
-      for (int i = 0; i < (n + 1) / 2 && !undone; i++) {
-        for (int j = 0; j < (n + 1) / 2 && !undone; j++) {
-          if (cellMapping[i][j] != map) continue;
-          while (counter < MAXN && countMember[counter] < cellMapping[i][j]) counter++;
-          if (counter == MAXN) {
-            undone = true;
-          } else {
-            a[i][j] = counter;
-            a[n - 1 - i][j] = counter;
-            a[i][n - 1 - j] = counter;
-            a[n - 1 - i][n - 1 - j] = counter;
-            countMember[counter] -= map;
+    int[][] ans = new int[n][n];
+    boolean OK = true;
+    for (int mapper : new int[] {4, 2, 1}) {
+      int current = 1;
+      for (int i = 0; i < (n + 1) / 2 && OK; i++) {
+        for (int j = 0; j < (n + 1) / 2 && OK; j++) {
+          if (cellMapping[i][j] != mapper) continue;
+          while (current < MAXN && counter[current] < mapper) current++;
+          if (current == MAXN) {
+            OK = false;
+            break;
           }
+          ans[i][j] = current;
+          ans[n - 1 - i][j] = current;
+          ans[i][n - 1 - j] = current;
+          ans[n - 1 - i][n - 1 - j] = current;
+          counter[current] -= mapper;
         }
       }
-      if (undone) break;
+      if (!OK) break;
     }
 
-    if (undone) {
-      out.println("NO");
-    } else {
+    if (OK) {
       out.println("YES");
       for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-          out.print(a[i][j] + " ");
+          out.print(ans[i][j] + " ");
         }
-        out.println();  
+        out.println();
       }
+    } else {
+      out.println("NO");
     }
 
     in.close();

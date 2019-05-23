4dd11f22cbad784bd77696688ecfc0ca11f54d79
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-05-23 13:27:14 +0700

    update template

--- COPY ME/B/Main.java
+++ COPY ME/B/Main.java
@@ -2,12 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  static Scanner in = new Scanner(System.in);
-  static PrintWriter out = new PrintWriter(System.out);
+  static InputStream inputStream = System.in;
+  static OutputStream outputStream = System.out;
+  static InputReader in = new InputReader(inputStream);
+  static PrintWriter out = new PrintWriter(outputStream);
   
   public static void main(String[] args) {
 
-    in.close();
     out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    public long nextLong () {
+      return Long.parseLong(next());
+    }
+
+    public char nextChar () {
+      return next().charAt(0);
+    }
   }
 }

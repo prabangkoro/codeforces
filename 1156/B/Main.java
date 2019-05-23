fc705c478f4d51f98f8e77a193f0939ac6be6f9b
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-05-23 16:03:16 +0700

    1156

--- 1156/B/Main.java
+++ 1156/B/Main.java
@@ -1,40 +1,98 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.*;
 
 public class Main {
-  static Scanner in = new Scanner(System.in);
-  static PrintWriter out = new PrintWriter(System.out);
+  static InputStream inputStream = System.in;
+  static OutputStream outputStream = System.out;
+  static InputReader in = new InputReader(inputStream);
+  static PrintWriter out = new PrintWriter(outputStream);
 
-  static void solve (String word)
-  {
-    char[] chars = word.toCharArray();
-    Arrays.sort(chars);
-    int half = chars.length / 2;
-    char[] merged = new char[chars.length];
-    for (int i = 0; i * 2 < chars.length; i++) {
-      out.println("i: " + i);
-      merged[2 * i] = chars[half + i];
-      merged[2 * i + 1] =  chars[i];
+  static boolean isOK (String query) {
+    boolean OK = true;
+    for (int i = 1; i < query.length() && OK; i++) {
+      if (Math.abs(query.charAt(i) - query.charAt(i - 1)) == 1) OK = false;
     }
-    String charsString = new String(chars);
-    String mergedString = new String(merged);
-    out.println("chars: " + charsString);
-    // out.println("merged: " + mergedString + "\n");
+    return OK;
   }
+
+  static void solve (String query) {
+
+    if (isOK(query)) {
+      out.println(query);
+    } else {
+      char[] querryArray = query.toCharArray();
+      ArrayList<Character> even = new ArrayList<>();
+      ArrayList<Character> odd = new ArrayList<>();
+
+      for (int i = 0; i < querryArray.length; i++) {
+        if (querryArray[i] % 2 == 0) even.add(querryArray[i]);
+        else odd.add(querryArray[i]);
+      }
+      Collections.sort(even);
+      Collections.sort(odd);
+
+      if (Math.abs(even.get(0) - odd.get(odd.size() - 1)) != 1) {
+        odd.addAll(even);
+        out.println(getStringRepresentation(odd));
+      } else if (Math.abs(odd.get(0) - even.get(even.size() - 1)) != 1) {
+        even.addAll(odd);
+        out.println(getStringRepresentation(even));
+      }
+      else out.println("No answer");
+    }
+  }
+  
   public static void main(String[] args) {
-    int n = in.nextInt();
-    in.nextLine();
+    int T = in.nextInt();
 
-    String[] words = new String[n];
-    for (int i = 0; i < n; i++) {
-      words[i] = in.nextLine();
+    for (int i = 0; i < T; i++) {
+      solve(in.next());
     }
 
-    for (int i = 0; i < n; i++) {
-      solve(words[i]);
-    }
-
-    in.close();
     out.close();
+  }
+
+  static String getStringRepresentation(ArrayList<Character> list)
+  {    
+    StringBuilder builder = new StringBuilder(list.size());
+    for(Character ch: list)
+    {
+      builder.append(ch);
+    }
+    return builder.toString();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    public long nextLong () {
+      return Long.parseLong(next());
+    }
+
+    public char nextChar () {
+      return next().charAt(0);
+    }
   }
 }

3f1eeaba8acdd79e06f8cbc309287ee88ff708ae
bagus prabangkoro <bagusprabangkoro@yahoo.co.id>
2019-09-07 09:22:31 +0700

    1169 A

--- 1169/A/Main.java
+++ 1169/A/Main.java
@@ -14,33 +14,20 @@ public class Main {
     int b = in.nextInt();
     int y = in.nextInt();
 
-    int distA = (a <= x) ? (x - a) : 2 * n - x - a;
-    int distB = (b >= y) ? (b - y) : b + y;
-    out.println("dist A: " + distA + " dist B: " + distB);
-
     boolean OK = false;
-    for (int i = 0; i <= distA && i <= distB && !OK; i++) {
-      int posA = a + i;
-      if (posA > n) posA = n - (posA - n);
-      int posB = b - i;
-      if (posB < 1) posB = -posB + 2;
-      out.println("pos A: " + posA + " pos B: " + posB);
-      if (posA == posB) OK = true;
-    }
 
-    // boolean OK = true;
-    // if (a > x ^ b > y) {
-    //   if ((a + b) % 2 != 0) OK = false;
-    //   else {
-    //     int mid = (a + b) / 2;
-    //     if (Math.abs(mid - a) == Math.abs(mid - b)) OK = true;
-    //     else OK = false;
-    //   }
-    // } else {
-    //   if (x >= Math.min(b, y) && x <= Math.max(b, y)) OK = true;
-    //   else if (y >= Math.min(a, x) && y <= Math.max(a, x)) OK = true;
-    //   else OK = false;
-    // }
+    while (a != x && b != y) {
+      if (a == b) {
+        OK = true;
+        break;
+      }
+      a++;
+      if (a > n) a = 1;
+      b--;
+      if (b < 1) b = n;
+    }
+    if (a == b) OK = true;
+
 
     out.println(OK ? "YES" : "NO");
 

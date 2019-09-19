9de6bddb6baf4d209fa68534285c3c952c501dad
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-09-19 19:48:33 +0700

    1220

---
+++ 1220/A/Main.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static InputStream inputStream = System.in;
+  static OutputStream outputStream = System.out;
+  static InputReader in = new InputReader(inputStream);
+  static PrintWriter out = new PrintWriter(outputStream);
+  
+  public static void main(String[] args) {
+    int n = in.nextInt();
+    int N = 0;
+    int Z = 0;
+    String str = in.next();
+
+    for (int i = 0; i < n; i++) {
+      char s = str.charAt(i);
+      if (s == 'n') N++;
+      else if (s == 'z') Z++;
+    }
+    while (N > 0) {
+      out.print("1 ");
+      N--;
+    }
+
+    while (Z > 0) {
+      out.print("0 ");
+      Z--;
+    }
+
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    public long nextLong () {
+      return Long.parseLong(next());
+    }
+
+    public char nextChar () {
+      return next().charAt(0);
+    }
+  }
+}
